ebe5c793331bae6236474f439ba7efb23a2e49af
aveal_cemik <aveal.cemik@gmail.com>
2025-12-18 12:12:24 +0200

    Посмотрел как работают компоненты View и Text

--- App.tsx
+++ App.tsx
@@ -1,108 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { useEffect, useRef, useState } from 'react';
-import { BackHandler, Button, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-    const [count, setCount] = useState(0);
-    const intervalRef = useRef<number | null>(null);
-
-    useEffect(() => {
-        return () => {
-            if (intervalRef.current !== null) {
-                clearInterval(intervalRef.current);
-            }
-        };
-    }, []);
-
-    const handleStartCount = () => {
-        if (intervalRef.current !== null) return;
-
-        intervalRef.current = setInterval(() => {
-            handleIncrementCount();
-        }, 1000) as unknown as number;
-    }
-
-    const handleStopCount = () => {
-        if (intervalRef.current !== null) {
-            clearInterval(intervalRef.current);
-            intervalRef.current = null;
-        };
-        handleResetCount();
-    };
-
-    const handlePauseCount = () => {
-        if (intervalRef.current !== null) {
-            clearInterval(intervalRef.current);
-            intervalRef.current = null;
-        };
-    };
-
-    const handleIncrementCount = () => {
-        setCount((prevCount) => {
-            return prevCount + 1;
-        });
-    }
-
-    const handleDecrementCount = () => {
-        setCount((prevCount) => {
-            return prevCount - 1;
-        })
-    }
-
-    const handleResetCount = () => {
-        setCount(0);
-    }
-
-    const handleExit = () => {
-        BackHandler.exitApp();
-    }
 
 
     return (
         <View style={styles.mainContainer}>
-            <View style={styles.container}>
-                <Text style={styles.text}>Счетчик: {count}</Text>
-                <View style={styles.wrapper}>
-                    <Button title='Старт' onPress={handleStartCount} />
-                    <Button title='Пауза' onPress={handlePauseCount} />
-                    <Button title='Стоп' onPress={handleStopCount} />
-                </View>
-                <View style={styles.wrapper}>
-                    <Button title="Увеличить" onPress={handleIncrementCount} />
-                    <Button title="Уменьшить" onPress={handleDecrementCount} />
-                    <Button title="Сбросить" onPress={handleResetCount} />
-                </View>
+            <View style={styles.primaryContainer}>
+                <Text style={styles.text}>Верхний блок</Text>
             </View>
-            <Button title='Выход' onPress={handleExit}/>
+
+            <View style={styles.secondaryContainer}>
+                <Text style={styles.text}>Средний блок</Text>
+            </View>
+
+            <View style={styles.threContainer}>
+                <Text style={styles.text}>Нижний блок</Text>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     mainContainer: {
-        marginTop: 30,
-        marginBottom: 50, 
-
         flex: 1,
-        gap: 5,
-
-        justifyContent: 'center',
-        alignItems: 'center',
+        backgroundColor: 'hsl(0, 0%, 10%)',
     },
-    container: {
+    primaryContainer: {
+        height: 80,
+
+        backgroundColor: 'hsl(0, 0%, 20%)',
+    },
+    secondaryContainer: {
         flex: 1,
-        gap: 5,
 
-        justifyContent: 'center',
-        alignItems: 'center',
+        backgroundColor: 'hsl(0, 0%, 30%)',
     },
-    wrapper: {
-        display: 'flex',
-        flexDirection: 'row',
-        gap: 5,
+    threContainer: {
+        height: 60,
+
+        backgroundColor: 'hsl(0, 0%, 40%)',
     },
     text: {
-        fontSize: 36,
-        fontWeight: 'bold'
+        color: 'hsl(0, 100%, 100%)',
     }
 });

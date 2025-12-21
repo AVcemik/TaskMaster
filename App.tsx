f4490c1583cd5d6729a5622ac99000fa34b6a282
aveal_cemik <aveal.cemik@gmail.com>
2025-12-21 22:54:00 +0200

    Мелкий фикс

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { StyleSheet } from 'react-native';
 import RootNavigator from './src/navigation/RootNavigator';
 
 export default function App() {
@@ -11,10 +10,3 @@ export default function App() {
         </NavigationContainer>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'hsl(0, 0%, 10%)',
-    }
-});
